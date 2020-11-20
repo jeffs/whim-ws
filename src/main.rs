fa039ecfbf9e447bb32c9136042da7763f4eba0c
Jeff Schwab <jeff@unbuggy.com>
2020-11-19 20:11:39 -0500

    Start debugging lack of support for encrypted key

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,7 @@ use std::env;
 #[tokio::main]
 async fn main() {
     // TODO: Add ArgParse.
-
     // TODO: Automate self-signed key gen.
-    //
-    // Init CA:
-    // * genpkey -> ca.key (rsa/des3 as pem)
-    // * req -> ca.pem (x509 as pem)
-    // * install...
-    //
-    // Generate site key and cert:
-    // * genrsa
-    // * generate_csr(
-    // * Cert::sign(Key) -> Cert
-
 
     let routes = warp::any().map(|| "Hello, world.");
     let args: Vec<_> = env::args().collect();
