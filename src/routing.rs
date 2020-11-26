a9a572919b8ba0c397a8382bced35fdaf9a872ab
Jeff Schwab <jeff@unbuggy.com>
2020-11-26 10:44:15 -0500

    Respect Accept-Encoding; return JSON
    Replace '/v0/hello' with '/v0/health'.

--- src/routing.rs
+++ src/routing.rs
@@ -1,5 +1,6 @@
 use crate::configuration::{HTTPS_PORT, HTTP_PORT};
-use percent_encoding::percent_decode_str;
+use crate::shell::{Shell, ShellID};
+use serde::Serialize;
 use std::convert::Infallible;
 use warp::http::uri::{Authority, PathAndQuery, Scheme};
 use warp::http::Uri;
@@ -7,9 +8,17 @@ use warp::path::FullPath;
 use warp::Rejection;
 use warp::{Filter, Reply};
 
-fn greet_param(param: String) -> String {
-    let name = percent_decode_str(&param).decode_utf8_lossy();
-    format!("Hello, {}.\n", name)
+#[derive(Debug, Serialize)]
+struct Health {
+    status: String,
+}
+
+impl Health {
+    fn fine() -> Health {
+        Health {
+            status: String::from("Fine, thanks for asking."),
+        }
+    }
 }
 
 pub fn http_routes() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
@@ -32,23 +41,31 @@ pub fn http_routes() -> impl Filter<Extract = (impl Reply,), Error = Rejection> 
         .with(warp::log("HTTP"))
 }
 
-fn get_shell(id: u32) -> String {
-    format!("shell #{}", id)
+fn get_shell(id: u32) -> impl Reply {
+    warp::reply::json(&Shell {
+        id: ShellID(id),
+        name: String::from("Default"),
+        history: Vec::new(),
+        columns: Vec::new(),
+    })
+}
+
+fn get_health() -> impl Reply {
+    warp::reply::json(&Health::fine())
 }
 
 pub fn https_routes() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
     // GET /               => web directory
-    // GET /v0/hello/      => Hello, world.
-    // GET /v0/hello/:name => Hello, {name}.
+    // GET /v0/health/     => json
     // GET /v0/shell/0     => default shell
-    let world = warp::path::end().map(|| "Hello, world.\n");
-    let param = warp::path::param().map(greet_param);
-    let hello = warp::path("hello").and(world.or(param));
+    let health = warp::path("health").map(get_health);
     let shell = warp::path!("shell" / u32).map(get_shell);
-    let api = warp::get().and(warp::path("v0")).and(hello.or(shell));
-    api.or(warp::fs::dir("web"))
-        .with(warp::compression::gzip())
-        .with(warp::log("HTTPS"))
+    let api = warp::get().and(warp::path("v0")).and(health.or(shell));
+    let uncompressed = api.or(warp::fs::dir("web")).with(warp::log("HTTPS"));
+    let compressed = warp::header::exact_ignore_case("accept-encoding", "gzip")
+        .and(uncompressed.clone())
+        .with(warp::compression::gzip());
+    compressed.or(uncompressed)
 }
 
 fn path_and_query() -> impl Filter<Extract = (PathAndQuery,), Error = Infallible> + Copy {
--- src/shell.rs
+++ src/shell.rs
@@ -1,19 +1,25 @@
 #![allow(dead_code)]
 
+use serde::Serialize;
 use serde_json::Value;
 use std::collections::HashMap;
 use std::path::PathBuf;
 use std::time::Duration;
 
-pub struct ShellID(u32);
-pub struct CardID(u32);
+#[derive(Debug, Serialize)]
+pub struct ShellID(pub u32);
 
+#[derive(Debug, Serialize)]
+pub struct CardID(pub u32);
+
+#[derive(Debug, Serialize)]
 pub enum Liveness {
     Static,
     Period(Duration),
 }
 
 // Metadata are normalized across card types
+#[derive(Debug, Serialize)]
 pub struct CardMeta {
     pub live: Liveness,
 }
@@ -23,6 +29,7 @@ pub struct CardMeta {
 // Note that argv and environment variables may hold arbitrary values, not only
 // strings.  OS-level environment variables may be heeded by the Whim server
 // itself, but card environments are decoupled.
+#[derive(Debug, Serialize)]
 pub struct Command {
     pub name: String, // Client-side class or function name
     pub argv: Value,  // Argument Value
@@ -36,6 +43,7 @@ pub struct Command {
 // Note that the output is an arbitrary, strictly evaluated value, not a stream
 // of bytes.  Live commands loaded in the client will make runtime
 // calls to the server API to update the output in real time.
+#[derive(Debug, Serialize)]
 pub struct Card {
     pub id: CardID,
     pub command: Command,
@@ -43,10 +51,12 @@ pub struct Card {
     pub output: Value,
 }
 
+#[derive(Debug, Serialize)]
 pub struct Column {
     pub cards: Vec<CardID>,
 }
 
+#[derive(Debug, Serialize)]
 pub struct Shell {
     pub id: ShellID,
     pub name: String,
