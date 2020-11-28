7b9f83cc8e90cc1f39038027cadd57e24035cf1d
Jeff Schwab <jeff@unbuggy.com>
2020-11-27 22:53:57 -0500

    Revert "Downgrade to Tokio 0.2; WebSockets kind of work"
    This reverts commit 8dbdc70b197afa62253ed76772e4c75ba6ae228d.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::process;
+use tokio_compat_02::FutureExt;
 use whim::{self, ClientPointer, Config, HOST_MASK, HTTPS_PORT, HTTP_PORT};
 
 async fn async_main() -> Result<(), Box<dyn Error>> {
@@ -12,16 +13,15 @@ async fn async_main() -> Result<(), Box<dyn Error>> {
         .key(whim::read_key(&config.tls).await?);
     println!("https://localhost:{}/", HTTPS_PORT);
     futures::join!(
-        http.run((HOST_MASK, HTTP_PORT)),
-        https.run((HOST_MASK, HTTPS_PORT))
+        http.run((HOST_MASK, HTTP_PORT)).compat(),
+        https.run((HOST_MASK, HTTPS_PORT)).compat()
     );
     Ok(())
 }
 
 fn main() {
     env_logger::init();
-    if let Err(err) = tokio::runtime::Builder::new()
-        .threaded_scheduler()
+    if let Err(err) = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .unwrap()
